d10e87a1847de7ab99ae1edbefc3d0732419af3b
xhad <chyad@hotmail.com>
2017-10-29 00:26:38 +0900

    first commit

---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var api = "http://localhost:8117";
+
+var coinUnits = 10000000000;
+
+var poolHost = "pool.crushcoin.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support@crushcoin.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
